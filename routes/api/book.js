f02df3676e53ddfe6465109d667f07d44404c687
AzureXX <ismat.valiyev@gmail.com>
2019-02-27 16:57:52 +0400

    book CRUD added

--- routes/api/book.js
+++ routes/api/book.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 const roles = require("../../utils/roles");
 const Book = require("../../models/Book");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 //@route   POST api/book/add
 //@desc    Adds new book to database
@@ -13,16 +14,20 @@ router.post(
   roles.isModerator,
   async (req, res, next) => {
     try {
-      const {nameEn, descriptionEn, authors, genres} = req.body;
+      const { nameEn, descriptionEn, authors, genres } = req.body;
       const newBook = new Book();
       newBook.name.en = nameEn;
       newBook.description.en = descriptionEn;
-      newBook.authors = authors.split(",").map(item => item.trim());
-      newBook.genres = genres.split(",").map(item => item.trim());
+      newBook.authors = authors
+        ? authors.split(",").map(item => item.trim())
+        : null;
+      newBook.genres = genres
+        ? genres.split(",").map(item => item.trim())
+        : null;
       const book = await newBook.save();
       res.status(200).json(book);
     } catch (error) {
-      next(error)
+      next(error);
     }
   }
 );
@@ -35,7 +40,24 @@ router.put(
   passport.authenticate("jwt", { session: false }),
   roles.isModerator,
   async (req, res, next) => {
-    res.json("Success");
+    const { nameEn, descriptionEn, authors, genres } = req.body;
+    try {
+      const id = new ObjectId(
+        ObjectId.isValid(req.params.id) ? req.params.id : "123456789012"
+      );
+      const book = await Book.findById(id);
+      if (!book) throw new Error("No such book exist");
+      book.name.en = nameEn;
+      book.description.en = descriptionEn;
+      book.authors = authors
+        ? authors.split(",").map(item => item.trim())
+        : null;
+      book.genres = genres ? genres.split(",").map(item => item.trim()) : null;
+      await book.save();
+      res.json("Success");
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
@@ -47,22 +69,54 @@ router.delete(
   passport.authenticate("jwt", { session: false }),
   roles.isModerator,
   async (req, res, next) => {
-    res.json("Success");
+    try {
+      if (!ObjectId.isValid(req.params.id)) throw new Error("ID is not valid");
+      await Book.findByIdAndDelete(req.params.id);
+      res.json("Success");
+    } catch (error) {
+      next(error);
+    }
   }
 );
 
 //@route   GET api/book/get/all/:page
 //@desc    Get all books by page
 //@access  Public
-router.get("/get/all/:page",  async (req, res, next) => {
-  res.json("Success");
-})
+router.get("/get/all/:page?", async (req, res, next) => {
+  try {
+    let page = parseInt(req.params.page);
+    const size = 3;
+    const offset = (page - 1) * size;
+    if (isNaN(page)) page = 1;
+    const books = await Book.find()
+      .skip(offset)
+      .limit(size);
+    if(books.length == 0) throw new Error("No such page"); 
+    if(books.length < size) res.json({lastPage: true, books});
+    res.json({lastPage: false, books});
+  } catch (error) {
+    next(error);
+  }
+});
+
+//@route   GET api/book/get/all/
+//@desc    Get all books by page
+//@access  Public
 
 //@route   GET api/book/get/id/:id
 //@desc    Get book by id
 //@access  Public
-router.get("/get/id/:id",  async (req, res, next) => {
-  res.json("Success");
-})
+router.get("/get/id/:id", async (req, res, next) => {
+  try {
+    const id = new ObjectId(
+      ObjectId.isValid(req.params.id) ? req.params.id : "123456789012"
+    );
+    const book = await Book.findById(id);
+    if (!book) throw new Error("No such book exist");
+    res.json(book);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
