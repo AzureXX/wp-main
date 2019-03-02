9d366e69fac24584905f785311523680cf94d268
AzureXX <ismat.valiyev@gmail.com>
2019-03-02 22:07:32 +0400

    added helmet js

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const passport = require("passport");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 const app = express();
 
+
+app.use(helmet())
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
