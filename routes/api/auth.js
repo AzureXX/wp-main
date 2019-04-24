553b66e0e2763fcc9259aff49be4f06e172a6c8b
AzureXX <ismat.valiyev@gmail.com>
2019-04-24 14:07:19 +0400

    100 items per page

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
+const Joi = require('Joi');
 
 //@route   POST api/auth/signup
 //@desc    Return JWT
 //@access  Public
 router.post('/signup', async(req, res, next) => {
-    const { username, email, password, password2, type} = req.body;
+    const { username, email, password, password2, type } = req.body;
 
     try {
         if (!password) throw new Error('Password is required');
         if (!email) throw new Error('Email is required');
-        if (password != password2) throw new Error("Passwords do not match")
+        if (password != password2) throw new Error('Passwords do not match');
 
         let user = await User.findOne({ email: req.body.email });
 
@@ -53,8 +54,46 @@ router.post('/signin', async(req, res, next) => {
     const { email, password } = req.body;
 
     try {
-        if (!password) throw new Error('Password is required');
-        if (!email) throw new Error('Email is required');
+        const schema = Joi.object().keys({
+            email: Joi.string()
+                .email()
+                .required()
+                .error(errors => {
+                    return errors.map(err => {
+                        switch (err.type) {
+                            case 'any.required':
+                                return { message: 'Email is required' };
+                            case 'string.email':
+                                return { message: 'Not a valid email' };
+                        }
+                    });
+                }),
+            password: Joi.string()
+                .required()
+                .error(errors => {
+                    return errors.map(err => {
+                        switch (err.type) {
+                            case 'any.required':
+                                return {
+                                    message: {
+                                        en: 'Password is required',
+                                        ru: 'Необходимо ввести пароль',
+                                        az: 'Parol mütləqdi'
+                                    }
+                                };
+                        }
+                    });
+                })
+        });
+
+        Joi.validate(req.body, schema, { abortEarly: false }, (err, value) => {
+            const errors = {};
+            err.details.forEach(el => {
+                errors[el.path[0]] = el.message;
+            });
+
+            if (err) throw new Error(JSON.stringify(errors));
+        });
 
         const user = await User.findOne({ email });
         if (!user) throw new Error('User Not Found');
@@ -73,6 +112,7 @@ router.post('/signin', async(req, res, next) => {
             throw new Error('Password is incorrect');
         }
     } catch (error) {
+        console.log(error);
         return next(error);
     }
 });
