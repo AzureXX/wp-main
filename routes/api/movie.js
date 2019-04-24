59baeacee347102c077d50c8df1207c54f9711c5
AzureXX <ismat.valiyev@gmail.com>
2019-04-24 14:20:32 +0400

    100 items per page

--- routes/api/movie.js
+++ routes/api/movie.js
@@ -85,7 +85,7 @@ router.delete(
 router.get('/get/all/:page?', async (req, res, next) => {
   try {
     let page = parseInt(req.params.page);
-    const size = 3;
+    const size = 100;
     if (isNaN(page)) page = 1;
     const offset = (page - 1) * size;
     if (isNaN(page)) page = 1;
