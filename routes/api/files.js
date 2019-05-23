ac12fb6fde16d83ad75774cfe26e26bc6c826422
AzureXX <ismat.valiyev@gmail.com>
2019-05-23 13:00:22 +0400

    fix

--- routes/api/files.js
+++ routes/api/files.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../../services/file-upload')
+const roles = require('../../utils/roles');
 
 const singleImageUpload = upload.single('image');
 
-router.post("/", (req,res, next) => {
+router.post("/",  roles.isAdmin, (req,res, next) => {
   singleImageUpload(req,res, (err) => {
     if(err) return console.log(err)
     console.log(req.file);
