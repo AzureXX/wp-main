cb25060d5e4b345e1cd44a9bec9ec631450f8fd9
AzureXX <ismat.valiyev@gmail.com>
2019-05-30 12:17:29 +0400

    vacancy model updated

--- models/Vacancy.js
+++ models/Vacancy.js
@@ -1,11 +1,26 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const VacancySchema = new Schema({
-  minAge: Number,
-  maxAge: Number,
-  creator: mongoose.Schema.Types.ObjectId
-})
+  education: String,
+  possition:String,
+  email: String,
+  phone: String,
+  ageMin: Number,
+  ageMax: Number,
+  requirements:[mongoose.Schema.Types.ObjectId],
+  workInfo:String,
+  companyName: String,
+  contactPerson: String,
+  experience: String,
+  salary: Number,
+  city: String,
+  category: String,
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
+});
 
-const Vacancy = mongoose.model("Vacancy", VacancySchema)
+const Vacancy = mongoose.model('Vacancy', VacancySchema);
 module.exports = Vacancy;
