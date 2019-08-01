356b995eef10214af7802ca1d45a748a3728fa47
AzureXX <ismat.valiyev@gmail.com>
2019-08-01 16:15:59 +0400

    search

--- routes/api/search.js
+++ routes/api/search.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
-const roles = require('../../utils/roles');
-const requests = require('../../utils/requests');
 const Book = require('../../models/Book');
 const Course = require('../../models/Course');
 const Movie = require('../../models/Movie');
 const Person = require('../../models/Person');
-const BookRating = require('../../models/Ratings/BookRating');
-const MovieRating = require('../../models/Ratings/MovieRating');
-const CourseRating = require('../../models/Ratings/CourseRating');
-const PersonRating = require('../../models/Ratings/PersonRating');
+const {getModel} = require("../../utils/transformation")
 
-router.get('/', roles.isUser, async (req, res, next) => {
+const searchOptions = (regex) => ([
+  { 'name.us': regex },
+  { 'name.ru': regex },
+  { 'name.az': regex }
+])
+router.get('/',  async (req, res, next) => {
   try {
     const regex = new RegExp(req.query.search, 'i');
     console.log(regex);
@@ -41,81 +40,29 @@ router.get('/', roles.isUser, async (req, res, next) => {
     movies = await movies;
     courses = await courses;
     people = await people;
-    const ratedBooks = [];
-    const ratedMovies = [];
-    const ratedCourses = [];
-    const ratedPeople = [];
-    if (req.user) {
-      let bookRating = BookRating.findOne({ userId: req.user.id });
-      let movieRating = MovieRating.findOne({ userId: req.user.id });
-      let courseRating = CourseRating.findOne({ userId: req.user.id });
-      let personRating = PersonRating.findOne({ userId: req.user.id });
-      bookRating = await bookRating;
-      movieRating = await movieRating;
-      courseRating = await courseRating;
-      personRating = await personRating;
 
-      books.forEach(item => {
-        const newItem = JSON.parse(JSON.stringify(item));
-        const index = bookRating
-          ? bookRating['books'].findIndex(i => {
-              return i.id.toString() === item._id.toString();
-            })
-          : -1;
-        if (index > -1) {
-          newItem.rating = bookRating['books'][index].rating;
-          newItem.status = bookRating['books'][index].status;
-        }
-        ratedBooks.push(newItem);
-      });
-
-      movies.forEach(item => {
-        const newItem = JSON.parse(JSON.stringify(item));
-        const index = movieRating
-          ? movieRating['movies'].findIndex(i => {
-              return i.id.toString() === item._id.toString();
-            })
-          : -1;
-        if (index > -1) {
-          newItem.rating = movieRating['movies'][index].rating;
-          newItem.status = movieRating['movies'][index].status;
-        }
-        ratedMovies.push(newItem);
-      });
-      courses.forEach(item => {
-        const newItem = JSON.parse(JSON.stringify(item));
-        const index = courseRating
-          ? courseRating['courses'].findIndex(i => {
-              return i.id.toString() === item._id.toString();
-            })
-          : -1;
-        if (index > -1) {
-          newItem.rating = courseRating['courses'][index].rating;
-          newItem.status = courseRating['courses'][index].status;
-        }
-        ratedCourses.push(newItem);
-      });
-
-      people.forEach(item => {
-        const newItem = JSON.parse(JSON.stringify(item));
-        const index = personRating
-          ? personRating['people'].findIndex(i => {
-              return i.id.toString() === item._id.toString();
-            })
-          : -1;
-        if (index > -1) {
-          newItem.rating = personRating['people'][index].rating;
-          newItem.status = personRating['people'][index].status;
-        }
-        ratedPeople.push(newItem);
-      });
-    }
     res.json({
-      books: ratedBooks.length > 0 ? ratedBooks : books,
-      movies: ratedMovies.length > 0 ? ratedMovies : movies,
-      courses: ratedCourses.length > 0 ? ratedCourses : courses,
-      people: ratedPeople.length > 0 ? ratedPeople : people
+      books: books,
+      movies: movies,
+      courses: courses,
+      people: people
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:type',  async (req, res, next) => {
+  try {
+    const types = ["books", "movies", "courses", "people","categories", "subcategories", "topics", "subtopics"]
+    if(!types.includes(req.params.type)) throw new Error("Invalid search type")
+    const regex = new RegExp(req.query.search, 'i');
+    const search = searchOptions(regex);
+    const Model = getModel(req.params.type)
+    let items = await Model.find({
+      $or: search
+    }, "name").limit(20);
+    res.json(items);
   } catch (error) {
     next(error);
   }
