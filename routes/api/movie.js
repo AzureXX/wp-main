71719e191c7c9d610bd157ae3e00feb94aa155a0
AzureXX <ismat.valiyev@gmail.com>
2019-09-07 23:25:34 +0400

    smaller tmdb images

--- routes/api/movie.js
+++ routes/api/movie.js
@@ -82,7 +82,7 @@ router.post(
         },
         duration: response.data.runtime,
         img: {
-          us: 'https://image.tmdb.org/t/p/original/' + response.data.poster_path
+          us: 'https://image.tmdb.org/t/p/w300/' + response.data.poster_path
         },
         released: response.data.release_date,
         genres: response.data.genres.map(genre => genre.name.toLowerCase())
