35ec4e24070153ef67922c84bf02b6e452755c3d
AzureXX <ismat.valiyev@gmail.com>
2019-10-28 16:23:24 +0400

    fix

--- models/Vacancy.js
+++ models/Vacancy.js
@@ -14,18 +14,21 @@ const VacancySchema = new Schema({
   contactPerson: String,
   subcategories: [
     {
+      _id: false,
       data: { type: mongoose.Schema.Types.ObjectId, ref: 'EducationSubcategory' },
       status: Number
     }
   ],
   topics: [
     {
+      _id: false,
       data: { type: mongoose.Schema.Types.ObjectId, ref: 'EducationTopic' },
       status: Number
     }
   ],
   subtopics: [
     {
+      _id: false,
       data: { type: mongoose.Schema.Types.ObjectId, ref: 'EducationSubtopic' },
       status: Number
     }
