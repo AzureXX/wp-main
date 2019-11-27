bbad84e06e79a86009fc80c7b3b09372433243f7
AzureXX <ismat.valiyev@gmail.com>
2019-11-27 16:15:58 +0400

    youtube api for music

--- routes/api/music.js
+++ routes/api/music.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const passport = require('passport');
 const roles = require('../../utils/roles');
 const requests = require('../../utils/requests');
+const Music = require("../../models/Music")
+const axios = require('axios');
 
 //@route   POST api/music/add
 //@desc    Adds new music to database
@@ -54,5 +56,43 @@ router.get('/get/id/:id',  async (req, res, next) => {
   await requests.getItem(req, res, next, 'music');
 });
 
+//@route   POST api/music/youtube
+//@desc    Add music using yyoutube api
+//@access  Public
+router.post('/youtube',  async (req, res, next) => {
+ try {
+   const {id} = req.body;
+   if (!id) throw new Error("id.required")
+   const response = await axios.get("https://www.googleapis.com/youtube/v3/videos", {
+     params: {
+       id: id,
+       part: "snippet",
+       key: process.env.YOUTUBE_API_KEY
+     }
+   })
+   const genres = req.body.genres
+        ? req.body.genres.split(',').map(i => i.trim().toLowerCase())
+        : null;
+      const tags = {}
+      if(genres) {
+        genres.forEach(genre => {
+          tags[genre.toLowerCase().split(" ").join("_")] = 3
+        })
+      }
+   const data = response.data.items[0]
+   const musicObj = new Music({
+     name: data.snippet.title,
+     video: id,
+     released: data.snippet.publishedAt,
+     img: data.snippet.thumbnails.medium.url,
+     genres: genres,
+     tags: tags
+   })
+   const music = await musicObj.save()
+   res.json(music);
+ } catch (error) {
+   next(error)
+ }
+});
 
 module.exports = router;
