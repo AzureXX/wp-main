ec289e5d24a6387c0d4f4e6d0e68dba926b2c8c1
AzureXX <ismat.valiyev@gmail.com>
2019-12-19 12:45:11 +0400

    Signup clossed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,78 +3,87 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
-const validator = require('../../validation/validators/authValidator')
+const validator = require('../../validation/validators/authValidator');
+
+const passport = require('passport');
+const roles = require('../../utils/roles');
 
 //@route   POST api/auth/signup
 //@desc    Return JWT
 //@access  Public
-router.post('/signup', async (req, res, next) => {
-  try {
-    const {
-      username,
-      email,
-      password,
-      type
-    } = req.body;
+router.post(
+  '/signup',
+  passport.authenticate('jwt', { session: false }),
+  roles.isAdmin,
+  async (req, res, next) => {
+    try {
+      const { username, email, password, type } = req.body;
 
-    // validate request body through hapi/joi signUp schema
-    validator.signUp(req.body)
+      // validate request body through hapi/joi signUp schema
+      validator.signUp(req.body);
 
-    let exist = await User.findOne({
-      email: req.body.email
-    }, "_id").lean();
+      let exist = await User.findOne(
+        {
+          email: req.body.email
+        },
+        '_id'
+      ).lean();
 
-    if (exist) throw new Error('email.exist');
-    if (username) {
-      exist = await User.findOne({
-        username: username
-      }, "_id").lean();
-      if (exist) throw new Error('user.exist');
+      if (exist) throw new Error('email.exist');
+      if (username) {
+        exist = await User.findOne(
+          {
+            username: username
+          },
+          '_id'
+        ).lean();
+        if (exist) throw new Error('user.exist');
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, salt);
+      const user = new User({
+        username: username,
+        email: email,
+        password: hash,
+        accountType: type
+      });
+      const newUser = await user.save();
+      const payload = {
+        id: newUser.id,
+        username: newUser.username,
+        role: newUser.role
+      };
+
+      const token = await jwt.sign(payload, process.env.SECRET_OR_KEY, {
+        expiresIn: 360000
+      });
+      return res.json({
+        success: true,
+        token: 'Bearer ' + token
+      });
+    } catch (error) {
+      next(error);
     }
-
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    const user = new User({
-      username: username,
-      email: email,
-      password: hash,
-      accountType: type
-    });
-    const newUser = await user.save();
-    const payload = {
-      id: newUser.id,
-      username: newUser.username,
-      role: newUser.role
-    };
-
-    const token = await jwt.sign(payload, process.env.SECRET_OR_KEY, {
-      expiresIn: 360000
-    });
-    return res.json({
-      success: true,
-      token: 'Bearer ' + token
-    });
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 //@route   POST api/auth/signin
 //@desc    Return JWT
 //@access  Public
 router.post('/signin', async (req, res, next) => {
   try {
-    const {
-      email,
-      password
-    } = req.body;
+    const { email, password } = req.body;
 
     // validate request body through hapi/joi signIn schema
-    validator.signIn(req.body)
+    validator.signIn(req.body);
 
-    const user = await User.findOne({
-      email
-    }, "+password username role").lean();
+    const user = await User.findOne(
+      {
+        email
+      },
+      '+password username role'
+    ).lean();
 
     if (!user) throw new Error('user.notfound');
     const isMatch = await bcrypt.compare(password, user.password);
