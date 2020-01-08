83eafa44abf02268076ddb6d2367fd845a201e5c
AzureXX <ismat.valiyev@gmail.com>
2020-01-08 13:03:30 +0400

    fix

--- services/recommendation.js
+++ services/recommendation.js
@@ -1,6 +1,7 @@
 const Limit = require("../models/Limit");
 
 module.exports.checkLimit = async (userID, recName) => {
+  return;
   let userRecLimit = await Limit.findOne({ userID });
   if (userRecLimit.recommendations[`${recName}`] <= 0) {
     throw new Error("user.recommendationLimit");
@@ -8,6 +9,7 @@ module.exports.checkLimit = async (userID, recName) => {
 };
 
 module.exports.changeLimit = async (userID, recommendationName, amount) => {
+  return;
   await Limit.updateOne(
     { userID },
     {
