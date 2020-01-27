363a2ceba17083cebbc18f38b785fae41456111d
AzureXX <ismat.valiyev@gmail.com>
2020-01-27 12:41:56 +0400

    added route for current user achievement

--- routes/api/achievement.js
+++ routes/api/achievement.js
@@ -1,29 +1,17 @@
-const express = require("express");
-const passport = require("passport");
-const requests = require("../../utils/requests");
-const roles = require("../../utils/roles");
-const axios = require("axios");
-
+const express = require('express');
+const passport = require('passport');
+const requests = require('../../utils/requests');
+const roles = require('../../utils/roles');
+const axios = require('axios');
+const passport = require('passport');
 const router = express.Router();
 //@route   GEt api/achievements/get/all
 //@desc    Get all achievements from database
 //@access  Public
-router.get("/get/all", async (req, res, next) => {
+router.get('/get/all', async (req, res, next) => {
   try {
-    const response = await axios.get(process.env.ACHIEVEMENTS_LINK + "get/all")
-    res.json(response.data)
-  } catch (error) {
-    next(error)
-  }
-});
-
-//@route   GEt api/achievements/get/id/:id
-//@desc    Get achievement by id from database
-//@access  Public
-router.get("/get/id/:id", async (req, res, next) => {
-  try {
-    const response = await axios.get(process.env.ACHIEVEMENTS_LINK + "get/id/"+ req.params.id)
-    res.json(response.data)
+    const response = await axios.get(process.env.ACHIEVEMENTS_LINK + 'get/all');
+    res.json(response.data);
   } catch (error) {
     next(error);
   }
@@ -32,16 +20,49 @@ router.get("/get/id/:id", async (req, res, next) => {
 //@route   GEt api/achievements/get/id/:id
 //@desc    Get achievement by id from database
 //@access  Public
-router.get("/user/:id", async (req, res, next) => {
+router.get('/get/id/:id', async (req, res, next) => {
   try {
-    const response = await axios.get(process.env.ACHIEVEMENTS_LINK + "user/get/id/"+ req.params.id)
-    res.json(response.data)
+    const response = await axios.get(
+      process.env.ACHIEVEMENTS_LINK + 'get/id/' + req.params.id
+    );
+    res.json(response.data);
   } catch (error) {
     next(error);
   }
 });
 
+//@route   GEt api/achievements/user/current
+//@desc    Get achievement by id from database
+//@access  Public
+router.get(
+  '/user/current',
+  passport.authenticate('jwt', {
+    session: false
+  }),
+  async (req, res, next) => {
+    try {
+      const response = await axios.get(
+        process.env.ACHIEVEMENTS_LINK + 'user/get/id/' + req.user._id
+      );
+      res.json(response.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
-
+//@route   GEt api/achievements/get/id/:id
+//@desc    Get achievement by id from database
+//@access  Public
+router.get('/user/id/:id', async (req, res, next) => {
+  try {
+    const response = await axios.get(
+      process.env.ACHIEVEMENTS_LINK + 'user/get/id/' + req.params.id
+    );
+    res.json(response.data);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
